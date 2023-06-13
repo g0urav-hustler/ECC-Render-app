2da868cc3780e053dabe6d7a44cc89b315b3cccf
g0urav-hustler <gouravchouhan966@gmail.com>
2023-06-13 18:32:25 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,44 +1,63 @@
+from tflite_runtime.interpreter import Interpreter
 import streamlit as st
 from PIL import Image
+import numpy as np
 
 model_path = "models/quantized_model.tflite"
+
+
+component_dict = {0: 'Ammature', 1: 'Automative fuse', 2: 'Battery', 3: 'Cartridge Fuse', 
+                    4: 'Ceramic Capacitor', 5: 'Clip Lead', 6: 'Crystals', 7: 'Diode',
+                    8: 'Electrolytic Capcitor', 9: 'Heat Sink', 10: 'Inductor', 11: 'Led',
+                    12: 'Microprocessor', 13: 'Mosfet', 14: 'Motor', 15: 'Potentiometer',
+                    16: 'Resistor', 17: 'Rheostat', 18: 'Shunt', 19: 'Switch',
+                    20: 'Transformer', 21: 'Transitor'}
 
 #title
 st.title("Electronic Component Scanner")
 
 #subtitle
-st.markdown("This application helps you to classify commponent by their images.")
+st.markdown("This application helps you to classify component by their images.")
 
-@st.cache_data(show_spinner="Loading the app..")
-def load_processor(model_name): 
-    processor = BlipProcessor.from_pretrained(model_name) 
-    return processor
+@st.cache_resource(show_spinner="Loading the app..")
+def load_model(model_name):
+    interpreter = Interpreter(model_path = model_name)
 
-@st.cache_data(show_spinner="App Loading..")  
-def load_model(model_name): 
-    model = BlipForConditionalGeneration.from_pretrained(model_name) 
-    return model
+    return interpreter
 
-processor = load_processor(model_name)
-model = load_model(model_name)
+
+interpreter = load_model(model_path)
+input_details = interpreter.get_input_details()
+  
+  
+input_index = input_details[0]["index"]
+image_shape = input_details[0]['shape'][1:-1]
+
 
 #image uploader
 image = st.file_uploader(label = "Upload your image here", type=['png','jpg','jpeg'])
 
 if image is not None:
 
-    input_image = Image.open(image) 
-    # st.image(input_image) #display image
-
+    img = Image.open(image) 
+    
     with st.spinner("Creating the captions... "):
+        img = img.resize(image_shape)
+        st.image(img)
+        input_tensor = np.array(np.expand_dims(img,0))
+        interpreter.allocate_tensors()
+        interpreter.set_tensor(input_index, input_tensor)
+        interpreter.invoke()
+        pred_output_details = interpreter.get_output_details()
 
-        raw_image = input_image.convert('RGB')
-        inputs = processor(raw_image, return_tensors="pt")
+        output_data = interpreter.get_tensor(pred_output_details[0]['index'])
+        pred = np.squeeze(output_data)
+        highest_pred_loc = np.argmax(pred)
 
-        out = model.generate(**inputs)
-        result = processor.decode(out[0], skip_special_tokens=True)
+        component_name = component_dict[highest_pred_loc]
 
-        st.write("You can use captions as - A beautiful photo of " + result)
+        
+        st.write("This is a ", component_name)
 else:
     st.write("Upload an Image first")
 
@@ -46,12 +65,5 @@ else:
 st.caption("Made by Gourav Chouhan ")
 
 
-
-
-
-    
-# if __name__ == "__main__":
-#     model_name = "Salesforce/blip-image-captioning-base"
-#     load_model(model_name)
   
 
